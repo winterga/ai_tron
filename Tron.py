83365a8a24cac823d438a702d295a1586d41f99c
winterga <greyson.a.wintergerst@vanderbilt.edu>
2024-11-27 13:46:13 -0600

    fixed game crash, Human implementation still needs fixing

--- Tron.py
+++ Tron.py
@@ -3,7 +3,7 @@ from GameBoard import GameBoard
 from MainMenu import MainMenu
 from GameOverMenu import GameOverMenu
 from Match import Match
-from Players import Player, Human, Computer
+from Players import Player, Human, Bot
 
 
 
@@ -97,11 +97,11 @@ class Tron:
 		#PVE - 1 human and 1 bot
         elif matchType == 1:
             self.players[1] = Human(self, (220, 0, 30), 1, 3, 3, Player.RIGHT, (pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_LEFT))
-            self.players[2] = Computer(self, (30, 220, 0), 2, self.board.xTiles-4, self.board.yTiles-4, Player.LEFT)
+            self.players[2] = Bot(self, (30, 220, 0), 2, self.board.xTiles-4, self.board.yTiles-4, Player.LEFT)
 		#EVE - 2 bots
         elif matchType == 2:
-            self.players[1] = Computer(self, (220, 0, 30), 1, 3, 3, Player.RIGHT)
-            self.players[2] = Computer(self, (90, 220, 50), 2, self.board.xTiles-4, self.board.yTiles-4, Player.LEFT)
+            self.players[1] = Bot(self, (220, 0, 30), 1, 3, 3, Player.RIGHT)
+            self.players[2] = Bot(self, (90, 220, 50), 2, self.board.xTiles-4, self.board.yTiles-4, Player.LEFT)
 
 		#Build match and set program state
         self.match = Match(self, matchType)
